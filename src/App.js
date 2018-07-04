65e3119d59e6146eaccaa65622b49349641a6106
seungprk@gmail.com <seungprk@gmail.com>
2018-07-03 23:51:42 -0700

    Start setup including threejs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import * as THREE from 'three';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+  }
+
+  componentDidMount() {
+    this.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
+    this.camera.position.z = 3000;
+    this.scene = new THREE.Scene();
+
+    this.renderer = new THREE.CSS3DRenderer({ canvas: this.canvas.current });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +25,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <canvas ref={this.canvas} />
       </div>
     );
   }
