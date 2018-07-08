2df12106820378eb33fc56c9be761e159f6d5326
seungprk@gmail.com <seungprk@gmail.com>
2018-07-07 21:51:07 -0700

    Create orbit with basic animation

--- src/3d/setup.js
+++ src/3d/setup.js
@@ -1,28 +1,49 @@
 import * as THREE from 'three';
 
+const createSphere = (radius, pos) => {
+  const geometry = new THREE.SphereGeometry(radius, 32, 32);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const sphere = new THREE.Mesh(geometry, material);
+  sphere.position.copy(pos);
+
+  return sphere;
+};
+
+const orbit = (sphere, radius, angle) => {
+  sphere.position.x = Math.cos(angle) * radius;
+  sphere.position.y = Math.sin(angle) * radius;
+};
+
 const setup = (canvas) => {
   this.camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     window.innerWidth / window.innerHeight,
-    0.1,
+    1,
     1000,
   );
-  this.camera.position.z = 5;
+  this.camera.position.z = 100;
   this.scene = new THREE.Scene();
 
   this.renderer = new THREE.WebGLRenderer({ canvas });
   this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-  const geometry = new THREE.SphereGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-  const cube = new THREE.Mesh(geometry, material);
-  this.scene.add(cube);
+  const sun = createSphere(5, new THREE.Vector3(0, 0, 0));
+  const planet1 = createSphere(1, new THREE.Vector3(15, 0, 0));
+  const planet2 = createSphere(1, new THREE.Vector3(25, 0, 0));
+  const planet3 = createSphere(1, new THREE.Vector3(35, 0, 0));
+  this.scene.add(sun);
+  this.scene.add(planet1);
+  this.scene.add(planet2);
+  this.scene.add(planet3);
 
+  let angle = 0;
   const animate = () => {
     requestAnimationFrame(animate);
     this.renderer.render(this.scene, this.camera);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    orbit(planet1, 15, angle);
+    orbit(planet2, 25, angle);
+    orbit(planet3, 35, angle);
+    angle += Math.PI / 360;
   };
   animate();
 };
