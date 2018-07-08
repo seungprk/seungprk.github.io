ece29bc4a180d613d2cc1198d6316f50ac58187b
seungprk@gmail.com <seungprk@gmail.com>
2018-07-07 20:31:57 -0700

    Show a low res rotating sphere

--- src/components/CanvasRenderer/CanvasRenderer.js
+++ src/components/CanvasRenderer/CanvasRenderer.js
@@ -9,12 +9,25 @@ class CanvasRenderer extends React.Component {
   }
 
   componentDidMount() {
-    this.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
-    this.camera.position.z = 3000;
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.z = 5;
     this.scene = new THREE.Scene();
 
     this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas.current });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const geometry = new THREE.SphereGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const cube = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      this.renderer.render(this.scene, this.camera);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+    };
+    animate();
   }
 
   render() {
