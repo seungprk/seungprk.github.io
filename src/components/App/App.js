7fe3237dc72867d8e21f173a9b2c36b1b19e7aa9
seungprk@gmail.com <seungprk@gmail.com>
2018-07-09 21:59:47 -0700

    Setup simple Modal component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from '../Modal/Modal';
 import CanvasRenderer from '../CanvasRenderer/CanvasRenderer';
 import logo from './logo.svg';
 import './App.css';
@@ -7,8 +8,11 @@ const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">
+        Welcome to React
+      </h1>
     </header>
+    <Modal />
     <CanvasRenderer />
   </div>
 );
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Modal.css';
+
+const Modal = () => (
+  <div className="Modal">
+    Modal here!
+  </div>
+);
+
+export default Modal;
