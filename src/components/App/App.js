97e57b518ce7e4b904ec53e6613b2f5e21b9ab61
seungprk@gmail.com <seungprk@gmail.com>
2018-07-09 22:35:06 -0700

    Abstract out pages to be displayed with keyboard controls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Modal from '../Modal/Modal';
 import CanvasRenderer from '../CanvasRenderer/CanvasRenderer';
+import pages from '../../pages/pages';
 import logo from './logo.svg';
 import './App.css';
-
-const MAX_PAGE = 3;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 0,
+      pageNum: 0,
     };
 
     this.handleKey = this.handleKey.bind(this);
@@ -19,16 +18,19 @@ class App extends React.Component {
   handleKey(e) {
     e.preventDefault();
 
-    let { page } = this.state;
-    if (e.key === 'ArrowUp') {
-      page = page >= MAX_PAGE ? MAX_PAGE : page + 1;
-    } else if (e.key === 'ArrowDown') {
-      page = page === 0 ? 0 : page - 1;
+    let { pageNum } = this.state;
+    const max = pages.length - 1;
+
+    if (e.key === 'ArrowDown') {
+      pageNum = pageNum >= max ? max : pageNum + 1;
+    } else if (e.key === 'ArrowUp') {
+      pageNum = pageNum === 0 ? 0 : pageNum - 1;
     }
-    this.setState({ page });
+    this.setState({ pageNum });
   }
 
   render() {
+    const { pageNum } = this.state;
     return (
       <div className="App" onKeyDown={this.handleKey} tabIndex="0">
         <header className="App-header">
@@ -38,7 +40,7 @@ class App extends React.Component {
           </h1>
         </header>
         <Modal>
-          Testing!
+          {pages[pageNum]}
         </Modal>
         <CanvasRenderer />
       </div>
---
+++ src/pages/page2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default (
+  <div>
+    Test Page 2
+  </div>
+);
