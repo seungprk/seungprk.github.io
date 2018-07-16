9d2cf967cfc3a4f1aae014bac2bbd12d2373a278
seungprk@gmail.com <seungprk@gmail.com>
2018-07-15 22:37:44 -0700

    Hide up and down arrows when on first or last page

--- src/components/Arrows/Arrows.js
+++ src/components/Arrows/Arrows.js
@@ -5,12 +5,22 @@ import downIcon from './down.svg';
 import './Arrows.css';
 
 const Arrows = ({ pageNum, max }) => {
+  let upClass = 'Arrows__icon';
+  if (pageNum === 0) upClass += ' Arrows__icon--hidden';
+  let downClass = 'Arrows__icon';
+  if (pageNum === max) downClass += ' Arrows__icon--hidden';
+
   return (
     <div className="Arrows">
-      <img className="Arrows__icon" src={upIcon} alt="Up" />
-      <img className="Arrows__icon" src={downIcon} alt="Down" />
+      <img className={upClass} src={upIcon} alt="Up" />
+      <img className={downClass} src={downIcon} alt="Down" />
     </div>
   );
+};
+
+Arrows.propTypes = {
+  pageNum: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
 };
 
 export default Arrows;
