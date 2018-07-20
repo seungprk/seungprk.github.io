84345ad552420ac6b21a11ee489128ea259b98d8
seungprk@gmail.com <seungprk@gmail.com>
2018-07-20 09:54:11 -0700

    Update particle size and add project content

--- src/3d/particleSystem.js
+++ src/3d/particleSystem.js
@@ -7,7 +7,7 @@ const vertexShader = `
   void main() {
     vAlpha = alpha;
     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-    gl_PointSize = 8.0;
+    gl_PointSize = 4.0;
     gl_Position = projectionMatrix * mvPosition;
   }
 `;
